e44dcd1f89b4908e22e9c0d2c2b93699560eea6f
klaus19 <tejaskhartude@gmail.com>
2022-09-19 17:16:25 +0530

    Added the classes

---
+++ app/src/main/java/com/example/currencyconverter/helper/EndPoints.kt
@@ -0,0 +1,17 @@
+package com.example.currencyconverter.helper
+
+class EndPoints {
+
+    companion object{
+
+        //BASE _URL
+
+        const val BASE_URL = "https://api.getgeoapi.com/api/v2/currency/"
+
+        const val API_KEY = "77dde19e901b963267990445b03f230a08ba9ce4"
+
+        //COVERT URL
+        const val  CONVERT_URL = "convert"
+
+    }
+}
